59330f71ab8836014161b54a8a1defbbceae2e5d
mickey9910326 <mickey9910326@gmail.com>
2017-12-08 18:38:31 +0800

    feature : Add ASA device defins

---
+++ ASA/ASA_DEVICE.h
@@ -0,0 +1,17 @@
+#ifndef __ASA_DEVICE_H__
+#define __ASA_DEVICE_H__
+
+#include "..\rev_DEVICE.h"
+
+#if defined (__AVR_ATmega128__)
+#include "ASA_M128.h"
+#elif defined (__AVR_ATmega88__)
+#include "ASA_M88.h"
+#else
+#  if !defined(__COMPILING_AVR_LIBC__)
+#    warning "device type not defined"
+#  endif
+#endif
+
+
+#endif
---
+++ ASA/ASA_M128.h
@@ -0,0 +1,22 @@
+#ifndef __ASA_M128_H__
+#define __ASA_M128_H__
+
+/**
+ * @brief Defines for serial
+ * usually use UART0 for serial
+ */
+#define ID_PIN  PINB
+#define ID_DDR  DDRB
+#define ID_PORT PORTB
+#define ID_MASK (1<<PB5)|(1<<PB6)|(1<<PB7)
+#define ID_SHIFT 5
+
+// ASA_SPI CS : PF4
+#define ASA_CS PF4
+#define ASA_CS_PIN  PINF
+#define ASA_CS_DDR  DDRF
+#define ASA_CS_PORT PORTF
+#define ASA_CS_MASK (1<<ASA_CS)
+#define ASA_CS_SHIFT PF4
+
+#endif /* __ASA_M128_H__ */
