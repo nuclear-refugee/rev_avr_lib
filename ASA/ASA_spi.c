e44853adfbcc507e805112cebbebfe79604e4356
mickey9910326 <mickey9910326@gmail.com>
2017-12-08 18:40:29 +0800

    feature : Add ASA spi uart protocol functino

---
+++ ASA/ASA_spi.c
@@ -0,0 +1,51 @@
+#include "ASA_spi.h"
+#include "ASA_general.h"
+
+char ASA_SPIM_trm(char isOneSLA, char isOneReg, char UARTID, char RegAdd, char Bytes, void *Data_p) {
+    if ( isOneSLA!=0 || isOneSLA!=1) return 1;
+    if ( isOneReg!=0 || isOneReg!=1) return 2;
+    // if ( RegAdd>127 ) return 3;
+
+    if ( !isOneSLA ) spi_swap(UARTID);
+    if ( !isOneReg ) spi_swap(RegAdd);
+    for (uint16_t i = 0; i < Bytes; i++) {
+        spi_swap( ((char*)Data_p)[i] );
+    }
+    return 0;
+};
+
+char ASA_SPIM_rec(char isOneSLA, char isOneReg, char UARTID, char RegAdd, char Bytes, void *Data_p) {
+    if ( isOneSLA!=0 || isOneSLA!=1) return 1;
+    if ( isOneReg!=0 || isOneReg!=1) return 2;
+    // if ( RegAdd>127 ) return 3;
+
+    if ( !isOneSLA ) spi_swap(UARTID);
+    if ( !isOneReg ) spi_swap(RegAdd);
+    for (uint16_t i = 0; i < Bytes; i++) {
+        ((char*)Data_p)[i] = spi_swap(255);
+    }
+    return 0;
+};
+
+char ASA_SPIM_ftm(char isOneSLA, char isOneReg, char UARTID, char RegAdd, char Mask, char shift, char Data) {
+    if ( isOneSLA!=0 || isOneSLA!=1) return 1;
+    if ( isOneReg!=0 || isOneReg!=1) return 2;
+    // if ( RegAdd>127 ) return 3;
+
+    char rec;
+    ASA_SPIM_rec(isOneSLA, isOneReg, UARTID, RegAdd, 1, &rec);
+    ASA_REGPUT(Data, rec, Mask, shift);
+    ASA_SPIM_trm(isOneSLA, isOneReg, UARTID, RegAdd, 1, &Data);
+    return 0;
+};
+
+char ASA_SPIM_frc(char isOneSLA, char isOneReg, char UARTID, char RegAdd, char Mask, char shift, char* Data_p) {
+    if ( isOneSLA!=0 || isOneSLA!=1) return 1;
+    if ( isOneReg!=0 || isOneReg!=1) return 2;
+    // if ( RegAdd>127 ) return 3;
+
+    char rec;
+    ASA_SPIM_rec(isOneSLA, isOneReg, UARTID, RegAdd, 1, &rec);
+    *Data_p = ASA_REGGET(rec, Mask, shift);
+    return 0;
+};
---
+++ ASA/ASA_spi.h
@@ -0,0 +1,23 @@
+#ifndef __ASA_SPI_H__
+#define __ASA_SPI_H__
+
+#include "../rev_spi.h"
+
+/**
+* @brief Replace of ASA_SPI_trm()
+*/
+char ASA_SPIM_trm(char isOneSLA, char isOneReg, char SPIID, char RegAdd, char Bytes, void *Data_p);
+/**
+ * @brief Replace of ASA_SPI_rec()
+ */
+char ASA_SPIM_rec(char isOneSLA, char isOneReg, char SPIID, char RegAdd, char Bytes, void *Data_p);
+/**
+* @brief Replace of ASA_SPI_ftm()
+*/
+char ASA_SPIM_ftm(char isOneSLA, char isOneReg, char SPIID, char RegAdd, char Mask, char shift, char Data);
+/**
+* @brief Replace of M128_SPI_frc()
+*/
+char ASA_SPIM_frc(char isOneSLA, char isOneReg, char SPIID, char RegAdd, char Mask, char shift, char* Data_p);
+
+#endif /* __ASA_SPI_H__ */
