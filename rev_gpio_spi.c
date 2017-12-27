b84950b1e424982f95104b1e53d395fd76a5811f
mickey9910326 <mickey9910326@gmail.com>
2017-12-28 00:10:45 +0800

    Add fun gspi swap

--- rev_gpio_spi.c
+++ rev_gpio_spi.c
@@ -1,9 +1,39 @@
 #include "rev_gpio_spi.h"
 #include "rev_DEVICE.h"
 #include "bit_op.h"
+#include "rev_spi.h"
+
+static enum spi_data_mode sspi_data_mode = mode_LRSL;
+
+void sspi_set_datamode(enum spi_data_mode mode) {
+    sspi_data_mode = mode;
+}
 
 // TODO inpleent SCK duration
 uint8_t sck_dur = 0;
+
+static inline void sck_high() {
+    BITSET(GSPI_PORT, PIN_GSPI_SCK);
+}
+static inline void sck_low() {
+    BITCLR(GSPI_PORT, PIN_GSPI_SCK);
+}
+static inline void mosi_high() {
+    BITSET(GSPI_PORT, PIN_MOSI);
+}
+static inline void mosi_low() {
+    BITCLR(GSPI_PORT, PIN_MOSI);
+}
+static inline void put_bit(uint8_t b) {
+    if(b){
+        mosi_high();
+    } else {
+        mosi_low();
+    }
+}
+static inline uint8_t get_bit() {
+    return BITCLR(GSPI_PORT, PIN_MISO);
+}
 
 void gspi_master_init() {
     BITSET(SPI_SPCR,MSTR);
@@ -43,27 +73,71 @@ void spi_reset_pulse(void) {
 }
 
 uint8_t gspi_swap(uint8_t data) {
-    uint8_t bitmask=128;
-    uint8_t res=0;
-    // software spi
-    while(bitmask!=0) {
-        // handle MOSI
-        if (data&bitmask) {
-            BITSET(GSPI_PORT, PIN_GSPI_MOSI); // transmit 1
-        } else {
-            BITCLR(GSPI_PORT, PIN_GSPI_MOSI); // transmit 0
+    uint8_t res = 0;
+    switch (sspi_data_mode) {
+        case mode_LRSL: {
+            put_bit(data&(1<<7));
+            sck_high();
+            res |= get_bit()<<7;
+            sck_low();
+            put_bit(data&(1<<6));
+            sck_high();
+            res |= get_bit()<<6;
+            sck_low();
+            put_bit(data&(1<<5));
+            sck_high();
+            res |= get_bit()<<5;
+            sck_low();
+            put_bit(data&(1<<4));
+            sck_high();
+            res |= get_bit()<<4;
+            sck_low();
+            put_bit(data&(1<<3));
+            sck_high();
+            res |= get_bit()<<3;
+            sck_low();
+            put_bit(data&(1<<2));
+            sck_high();
+            res |= get_bit()<<2;
+            sck_low();
+            put_bit(data&(1<<1));
+            sck_high();
+            res |= get_bit()<<1;
+            sck_low();
+            put_bit(data&(1<<0));
+            sck_high();
+            res |= get_bit()<<0;
+            sck_low();
+            return res;
         }
-        _delay_loop_1(sck_dur);
-        // handle MISO
-        if(GSPI_PORT & (1<<PIN_GSPI_MISO)){
-            res |= bitmask;
-        }
-        // handle SCK
-        BITSET(GSPI_PORT, PIN_GSPI_SCK); // SCK high
-        _delay_loop_1(sck_dur);
-        _delay_loop_1(3); // make low and high pulse equaly long
-        bitmask = bitmask>>1;
-        BITCLR(GSPI_PORT, PIN_GSPI_SCK); // SCK low
+        case mode_LRST:
+            break;
+        case mode_LFSL:
+            break;
+        case mode_LFST:
+            break;
     }
-    return(res);
+    // uint8_t bitmask=128;
+    // uint8_t res=0;
+    // // software spi
+    // while(bitmask!=0) {
+    //     // handle MOSI
+    //     if (data&bitmask) {
+    //         BITSET(GSPI_PORT, PIN_GSPI_MOSI); // transmit 1
+    //     } else {
+    //         BITCLR(GSPI_PORT, PIN_GSPI_MOSI); // transmit 0
+    //     }
+    //     _delay_loop_1(sck_dur);
+    //     // handle MISO
+    //     if(GSPI_PORT & (1<<PIN_GSPI_MISO)){
+    //         res |= bitmask;
+    //     }
+    //     // handle SCK
+    //     BITSET(GSPI_PORT, PIN_GSPI_SCK); // SCK high
+    //     _delay_loop_1(sck_dur);
+    //     _delay_loop_1(3); // make low and high pulse equaly long
+    //     bitmask = bitmask>>1;
+    //     BITCLR(GSPI_PORT, PIN_GSPI_SCK); // SCK low
+    // }
+    // return(res);
 }
